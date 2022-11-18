718532a4eafab2a2ffd7290ed028a1ed9a69ac0c
Nicolas <nicolasgabriel.developer@gmail.com>
2022-11-17 21:53:57 -0300

    Modificando todos os Stats individuais dos pokemons

--- js/main.js
+++ js/main.js
@@ -48,26 +48,48 @@ Promise.all(pokemonPromises)
       const types = pokemon.types.map(typeInfo => typeInfo.type.name)
       const typesSeparadas = types.join(' ');
       const type = typesSeparadas.split(' ')
-     
+      const namePokemons = pokemon.name
     
+   
       
       accumulator += `
       <li class="container-pokemon">
       <div class="statushover">
           <div class="status ${type[0]}">
-          <p class="base-titulo">BASE STATS</p>
-          <div>
-          <p class="status-individual"> HP: ${pokemon.stats[0].base_stat}</p>
-          <p class="status-individual"> Attack: ${pokemon.stats[1].base_stat}</p>
-          <p class="status-individual"> Defense: ${pokemon.stats[2].base_stat}</p>
-          <p class="status-individual"> Special Attack: ${pokemon.stats[3].base_stat}</p>
-          <p class="status-individual"> Special Defense: ${pokemon.stats[4].base_stat}</p>
-          <p class="status-individual"> Speed: ${pokemon.stats[5].base_stat}</p>
+          <p class="base-titulo">STATS</p>
+          <div class="caixa-status">
+          <div class="esquerda">
+          <p class="status-individual"> HP: </p>
+          <p class="status-individual"> Attack:</p>
+          <p class="status-individual"> Defense:</p>
+          <p class="status-individual"> Special Attack:</p>
+          <p class="status-individual"> Special Defense:</p>
+          <p class="status-individual"> Speed:</p>
+          </div>
+          <div class="direita">
+          <div class="container-status">
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[0].base_stat}%/2);"></div>
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[1].base_stat}%/2);"></div>
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[2].base_stat}%/2);"></div>
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[3].base_stat}%/2);"></div>
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[4].base_stat}%/2);"></div>
+              <div class="baixo"></div>
+              <div class="cima" style="width: calc(${pokemon.stats[5].base_stat}%/2);"></div>
+          </div>
+
+          </div><!--direita-->
+          
+        
           </div>
       </div>
           <a  class="resposta ${pokemon.name}" ><img class="imagem-pokemon" alt"${pokemon.name} src="${pokemon['sprites']['other']['official-artwork']['front_default']}"/></a>
           <p class="numero-pokemon">Nº ${pokemon.id.toString().padStart(3, '0')}</p>
-          <p class="nome-pokemon">${pokemon.name}</p>
+          <p class="nome-pokemon"  >${namePokemons}</p>
           <div class="container-tipo">
             <div class="tipo1 ${type[0]}">${type[0]}</div>
             <div class="tipo2 ${type[1]}">${type[1]}</div>
